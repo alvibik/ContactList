d2a9ca21c9a315e8823f1e2338b18238fdd0be28
Александр Биктеев <bikteev@mail.ru>
2022-11-05 17:23:51 +0600

    1. Setup model 2. Setup service for model

---
+++ ContactList/Models/Persons.swift
@@ -0,0 +1,36 @@
+//
+//  Persons.swift
+//  ContactList
+//
+//  Created by albik on 05.11.2022.
+//
+
+import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+    let email: String
+    let phone: String
+    
+    static func getPerson() -> [Person] {
+        var persons : [Person] = []
+        
+        let firstNames = DataStore().firstNames.shuffled()
+        let lastNames = DataStore().lastNames.shuffled()
+        let emails = DataStore().emails.shuffled()
+        let phones = DataStore().phones.shuffled()
+        
+        for i in 0..<firstNames.count {
+            let person = Person(
+                firstName: firstNames[i],
+                lastName: lastNames[i],
+                email: emails[i],
+                phone: phones[i]
+            )
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
---
+++ ContactList/Services/DataStore.swift
@@ -0,0 +1,15 @@
+//
+//  DataStore.swift
+//  ContactList
+//
+//  Created by albik on 05.11.2022.
+//
+
+import Foundation
+
+final class DataStore {
+    let firstNames = ["Elijah", "Ian", "Sean"]
+    let lastNames = ["Wood", "McKellen", "Astin"]
+    let emails = ["Frodo@lor.org", "Gandalf@lor.org", "Sam@lor.org"]
+    let phones = ["+7 (999) 888-77-66", "+7 (888) 777-66-55", "+7 (777) 666-55-44"]
+}
